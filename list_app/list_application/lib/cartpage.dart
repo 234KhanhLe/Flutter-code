f7555a3f3c5a33197403413dce8c77ef703d84ff
234KhanhLe <caokhanhle145@gmail.com>
2024-04-09 00:21:05 +0700

    commit bottom nagivator and fix some classes

--- list_app/list_application/lib/cartpage.dart
+++ list_app/list_application/lib/cartpage.dart
@@ -4,50 +4,71 @@ import 'package:list_application/draganddrop.dart';
 class CartPage extends StatelessWidget {
   final List<Customer> _customer;
 
-  const CartPage({super.key, required List<Customer> customer})
+  CartPage({super.key, required List<Customer> customer})
       : _customer = customer;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Cart'),
+        title: const Text('Orders List'),
       ),
-      body: ,
+      body: ListView.builder(
+        itemCount: _customer.length,
+        itemBuilder: (context, index) {
+          return _buildCartList(_customer[index]);
+        },
+      ),
     );
   }
 
-  Widget _buildCartList(){
-    return ListView.builder(
-      itemCount: _customer.length,
-      itemBuilder: (context, index) {
-      final customer = _customer[index];
-      if(customer.items.isEmpty){
-        return ListTile(
-          title: Text(customer.name),
-          subtitle: const Text('cart is empty'),
-        );
-      } else{
-        return ;
-      }
-      },
-      );
+  Widget _buildCartList(Customer customer) {
+    final num totalPriceOfEachCustomer = customer.items.isNotEmpty
+        ? customer.items.map((item) => item.totalPrices).reduce((a, b) => a + b)
+        : 0.0;
+    return Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text(customer.name),
+            subtitle: customer.items.isEmpty
+                ? const Text(
+                    'No order',
+                    style: TextStyle(color: Colors.red),
+                  )
+                : Text('${customer.items.length} items'),
+          ),
+          if (customer.items.isNotEmpty)
+            Column(
+              children: [
+                for (final item in customer.items)
+                  ListTile(
+                    title: Text(item.name),
+                    subtitle: Text(
+                        '\$${(item.totalPrices / 100.0).toStringAsFixed(2)}',
+                        style: const TextStyle(color: Colors.lightGreen)),
+                  ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Total: '),
+                  subtitle: Text(
+                    '\$${(totalPriceOfEachCustomer / 100.0).toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ],
+            )
+        ],
+      ),
+    );
   }
-
-  Widget _buildCustomCartListItem({required Customer customer}){
-    List<Item> customerItemList = customer.items;
-    return ListView.separated(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, index) {
-        return const SizedBox(
-          height: 12,
-        );
-      }, 
-      separatorBuilder:(context, index) {
-        final item = customerItemList[index];
-        return 
-      }, 
-      itemCount: customerItemList.length);
-  }
-
 }
