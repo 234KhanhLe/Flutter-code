10dfaa620132a1a5b6ff2dbad78a901fad1f4dff
234KhanhLe <caokhanhle145@gmail.com>
2024-04-08 20:46:24 +0700

    commit all

---
+++ list_application/lib/draganddrop.dart
@@ -0,0 +1,384 @@
+import 'package:flutter/material.dart';
+
+class TotalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ExampleDragAndDrop(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+@immutable
+class Item {
+  final int totalPrices;
+  final String name;
+  final String uid;
+  final String imagePath;
+  const Item({
+    required this.totalPrices,
+    required this.name,
+    required this.uid,
+    required this.imagePath,
+  });
+
+  String get formattedTotalPrices =>
+      '\$${(totalPrices / 100.0).toStringAsFixed(2)}';
+}
+
+class Customer {
+  final String name;
+  final String imagePath;
+  final List<Item> items;
+
+  Customer({
+    required this.name,
+    required this.imagePath,
+    List<Item>? items,
+  }) : items = items ?? [];
+
+  String get formatTotalPrices {
+    final totalPriceCents =
+        items.fold<int>(0, (prev, item) => prev + item.totalPrices);
+    return '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
+  }
+}
+
+const List<Item> _items = [
+  Item(totalPrices: 100, name: 'Avatar A', uid: '1', imagePath: 'images/D.png'),
+  Item(totalPrices: 200, name: 'Avatar B', uid: '2', imagePath: 'images/E.png'),
+  Item(totalPrices: 300, name: 'Avatar C', uid: '3', imagePath: 'images/F.png'),
+];
+
+@immutable
+class ExampleDragAndDrop extends StatefulWidget {
+  const ExampleDragAndDrop({super.key});
+  @override
+  State<ExampleDragAndDrop> createState() => _ExampleDragAndDropState();
+}
+
+class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
+    with TickerProviderStateMixin {
+  final List<Customer> _people = [
+    Customer(name: 'Customer D', imagePath: 'images/A.jpg'),
+    Customer(name: 'Customer E', imagePath: 'images/B.jpg'),
+    Customer(name: 'Customer F', imagePath: 'images/C.jpg')
+  ];
+
+  final GlobalKey _draggableKey = GlobalKey();
+  void _itemDroppedOnCustomerCart({
+    required Item item,
+    required Customer customer,
+  }) {
+    setState(() {
+      customer.items.add(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F7F7),
+      appBar: _buildAppBar(),
+      body: _buildContent(),
+      bottomNavigationBar: _bottomNavigationBar(),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      iconTheme: const IconThemeData(color: Color(0xFFF64209)),
+      title: Text(
+        'Order Food',
+        style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              fontSize: 36,
+              color: const Color(0xFFF64209),
+              fontWeight: FontWeight.bold,
+            ),
+      ),
+      backgroundColor: const Color(0xFFF7F7F7),
+      elevation: 0,
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildContent() {
+    return Stack(
+      children: [
+        SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: _buildMenuList(),
+              ),
+              _buildPeopleRow(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMenuList() {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: _items.length,
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          height: 12,
+        );
+      },
+      itemBuilder: (context, index) {
+        final item = _items[index];
+        return _buildMenuItem(
+          item: item,
+        );
+      },
+    );
+  }
+
+  Widget _buildMenuItem({
+    required Item item,
+  }) {
+    return LongPressDraggable(
+      data: item,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: DraggingListItem(
+        dragKey: _draggableKey,
+        imagePath: item.imagePath,
+      ),
+      child: MenuListItem(
+        imagePath: item.imagePath,
+        name: item.name,
+        price: item.formattedTotalPrices,
+      ),
+    );
+  }
+
+  Widget _buildPeopleRow() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 20,
+      ),
+      child: Row(
+        children: _people.map(_buildPersonWithDropZone).toList(),
+      ),
+    );
+  }
+
+  Widget _buildPersonWithDropZone(Customer customer) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 6,
+        ),
+        child: DragTarget<Item>(
+          builder: (context, candidateItem, rejectedItem) {
+            return CustomerCart(
+                hasItems: customer.items.isNotEmpty,
+                highlighted: candidateItem.isNotEmpty,
+                customer: customer);
+          },
+          onAcceptWithDetails: (details) {
+            _itemDroppedOnCustomerCart(item: details.data, customer: customer);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart), label: 'My Cart'),
+      ],
+      currentIndex: 0,
+    );
+  }
+}
+
+class CustomerCart extends StatelessWidget {
+  const CustomerCart({
+    super.key,
+    required this.customer,
+    this.highlighted = false,
+    this.hasItems = false,
+  });
+
+  final Customer customer;
+  final bool highlighted;
+  final bool hasItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = highlighted ? Colors.white : Colors.black;
+
+    return Transform.scale(
+      scale: highlighted ? 1.075 : 1.0,
+      child: Material(
+        elevation: highlighted ? 8 : 4,
+        borderRadius: BorderRadius.circular(22),
+        color: highlighted ? const Color(0xFFF64209) : Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 24,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipOval(
+                child: SizedBox(
+                  width: 46,
+                  height: 46,
+                  child: Image.asset(
+                    customer.imagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                customer.name,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: textColor,
+                      fontWeight:
+                          hasItems ? FontWeight.normal : FontWeight.bold,
+                    ),
+              ),
+              Visibility(
+                visible: hasItems,
+                maintainState: true,
+                maintainAnimation: true,
+                maintainSize: true,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 4),
+                    Text(
+                      customer.formatTotalPrices,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: textColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '${customer.items.length}item${customer.items.length != 1 ? 's' : ''}',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: textColor,
+                            fontSize: 12,
+                          ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuListItem extends StatelessWidget {
+  final String name;
+  final String price;
+  final String imagePath;
+  final bool isDepressed;
+
+  const MenuListItem(
+      {super.key,
+      this.name = '',
+      this.price = '',
+      required this.imagePath,
+      this.isDepressed = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 12,
+      borderRadius: BorderRadius.circular(20),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeInOut,
+                    height: isDepressed ? 115 : 120,
+                    width: isDepressed ? 115 : 120,
+                    child: Image.asset(
+                      imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DraggingListItem extends StatelessWidget {
+  final GlobalKey dragKey;
+  final String imagePath;
+
+  const DraggingListItem(
+      {super.key, required this.dragKey, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionalTranslation(
+      translation: const Offset(-0.5, -0.5),
+      child: ClipRRect(
+        key: dragKey,
+        borderRadius: BorderRadius.circular(12),
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Opacity(
+            opacity: 0.85,
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(TotalPage());
+}
